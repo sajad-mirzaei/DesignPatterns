1ddabb97bc12cd0bcfb435e2802f44e061c0ed30
Sajad <mirzaei.sajad@hotmail.com>
2022-07-06 18:42:26 +0430

    upload Singleton pattern

---
+++ src/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using Singleton.Struct1;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            SingletonClass s1 = SingletonClass.GetInstance();
+            SingletonClass s2 = SingletonClass.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
