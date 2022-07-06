88cd456d184980e7678c76c6591de43ba05cf3f4
Sajad <mirzaei.sajad@hotmail.com>
2022-07-06 18:21:22 +0430

    upload Proxy pattern

---
+++ src/Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using Proxy.Struct1;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            Console.WriteLine("Client: Executing the client code with a real subject:");
+            RealSubject realSubject = new RealSubject();
+            client.ClientCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:");
+            ProxyClass proxy = new ProxyClass(realSubject);
+            client.ClientCode(proxy);
+        }
+    }
+}
---
+++ src/Proxy/Struct1/RealSubject.cs
@@ -0,0 +1,14 @@
+﻿namespace Proxy.Struct1
+{
+    // The RealSubject contains some core business logic. Usually, RealSubjects
+    // are capable of doing some useful work which may also be very slow or
+    // sensitive - e.g. correcting input data. A Proxy can solve these issues
+    // without any changes to the RealSubject's code.
+    class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("RealSubject: Handling Request.");
+        }
+    }
+}
